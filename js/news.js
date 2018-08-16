1a91e7ddcb69b6bbca854ede57bac362d5d7d822
Шмелев Иван <ishmelev@1ditis.ru>
2018-08-16 18:38:08 +0300

    Сделал страницу новостей, верстка генерируется процедурно через скрипт news.js. Добавил в общие стили классы для заголовков и текста.

---
+++ js/news.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    for(i=0; i<2; i++){
+        $(".container-fluid").append('<div class="news-block"></div>');
+    }
+    $(".news-block").html('<div class="row"><div class="col-xl-6"><h2 class="head-h2">"Кубок хвостатого-2018"</h2></div></div><div class="row"><div class="col-xl-6"><p class="text-p text-p-news">20.08.2018</p></div></div><div class="row"><div class="col-xl-6"><img class="news-image" src="img/news1.jpg"></div><div class="col-xl-6"><div class="news-block-text"><p class="text-p">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus laborum id earum sint, quod, praesentium maxime magni dolores corrupti ut obcaecati placeat. Deleniti commodi debitis hic quo, eum sequi! Officia.</p></div></div</div>');
+});
---
+++ news.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css"  type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="js/news.js"></script>
+    <title>Мероприятия</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="news-block">
+            <!-- <div class="row">
+                <div class="col-xl-6">
+                    <h2 class="head-h2">"Кубок хвостатого-2018"</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-6">
+                    <p class="text-p text-p-news">20.08.2018</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-6">
+                    <img class="news-image" src="img/news1.jpg">
+                </div>
+                <div class="col-xl-6">
+                    <div class="news-block-text">
+                        <p class="text-p">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus laborum id earum sint, quod, praesentium maxime magni dolores corrupti ut obcaecati placeat. Deleniti commodi debitis hic quo, eum sequi! Officia.</p>
+                    </div>
+                </div>
+            </div> -->
+        </div>    
+    </div>
+</body>
+</html>
